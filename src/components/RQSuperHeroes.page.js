2e96eca8dd18b9b9e31407ad01a7ecd2c7aee39b
Mehari David <david.mehari@gmail.com>
2022-10-29 13:01:14 +0200

    basic error handling adde

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,7 +1,25 @@
-const RQSuperHeroesPage = () => {
-  return (
-    <div>RQSuperHeroesPage</div>
-  )
-}
+import { useQuery } from 'react-query';
+import axios from 'axios';
 
-export default RQSuperHeroesPage
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes');
+};
+
+export const RQSuperHeroesPage = () => {
+  const { isLoading, data, isError, error } = useQuery(
+    'super-heroes',
+    fetchSuperHeroes
+  );
+
+  if (isLoading) return <h2>Loading...</h2>;
+  if (isError) return <h2>{error.message}</h2>;
+
+  return (
+    <>
+      <h2>RQSuperHeroesPage</h2>
+      {data?.data.map((hero) => (
+        <div key={hero.id}>{hero.name}</div>
+      ))}
+    </>
+  );
+};
